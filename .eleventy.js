eb45276fb6b8d7c23ad2e6de6b68744d8f8bc49f
Alex Rønne Petersen <alex@alexrp.com>
2024-01-23 00:24:49 +0100

    Add @11ty/eleventy-plugin-directory-output.

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
 /** @param {import("@11ty/eleventy").UserConfig} config */
 module.exports = function(config) {
+    config.addPlugin(require("@11ty/eleventy-plugin-directory-output"));
+
     config.addPassthroughCopy({
         "arise.*": "images",
         "src/images": ""
