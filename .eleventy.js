903c6d696acf3b0be659c697d9127b0181d97592
Alex Rønne Petersen <alex@alexrp.com>
2024-02-17 12:34:49 +0100

    Add Eleventy syntax highlighting plugin.

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 /** @param {import("@11ty/eleventy").UserConfig} config */
 module.exports = function(config) {
     config.addPlugin(require("@11ty/eleventy-plugin-directory-output"));
+    config.addPlugin(require("@11ty/eleventy-plugin-syntaxhighlight"));
 
     config.addPassthroughCopy({
         "arise.*": "images",
